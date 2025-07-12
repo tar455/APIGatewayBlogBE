e7ff86d18612d6bef2da9c2004f77361c71f5ef2
tar455 <tariqueansari455@gmail.com>
2025-07-13 00:25:53 +0530

    Init commit

---
+++ src/main/java/com/tariq/ApiGatewayBlogBE/ApiGatewayBlogBeApplication.java
@@ -0,0 +1,14 @@
+package com.tariq.ApiGatewayBlogBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = "com.tariq")
+public class ApiGatewayBlogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayBlogBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tariq/ApiGatewayBlogBE/ApiGatewayBlogBeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tariq.ApiGatewayBlogBE;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayBlogBeApplicationTests {
+
+	@Test
+	@Disabled("Disable until monday")
+	void contextLoads() {
+	}
+
+}
