e98c13e63e580859e744fc6c8f59adb640af0066
tar455 <tariqueansari455@gmail.com>
2025-10-02 22:53:48 +0530

    Tarique: Swagger fix

--- src/main/java/com/tariq/config/SecurityConfig.java
+++ src/main/java/com/tariq/config/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.tariq.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -15,7 +13,12 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         return http.csrf(ServerHttpSecurity.CsrfSpec::disable).
                 authorizeExchange(auth ->
-                        auth.pathMatchers("/BlogBE/v3/api-docs/**", "/swagger-ui/**", "/v3/api-docs/**", "/webjars/swagger-ui/index.html").
+                        auth.pathMatchers("/BlogBE/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/webjars/swagger-ui/index.html/**",
+                                        "/webjars/**",
+                                        "/").
                                 permitAll().anyExchange().authenticated()).formLogin(ServerHttpSecurity.FormLoginSpec::disable).httpBasic(ServerHttpSecurity.HttpBasicSpec::disable).build();
 
     }
